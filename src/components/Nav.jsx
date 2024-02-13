d138bc3f6459d6b8385b7f6a889335c6c38144a0
emilyTheseond <esegundo02@gmail.com>
2024-02-13 13:50:22 -0600

    trash can

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,22 +3,25 @@ import { NavLink } from 'react-router-dom';
 
 const Nav = ({ user, handleLogOut }) => {
     return (
-        <div className='navbar'>
-            {/* change to the strokes logo */}
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/songs'>Music</NavLink>
-            <NavLink to='/band'>The Band</NavLink>
-            <NavLink to='/boards'>Boards</NavLink>
-            <NavLink to='/shows'>Shows</NavLink>
-            {user ? (
-                <>
-                    <button onClick={handleLogOut}>Logout</button>
-                </>
-            ) : (
-                <>
-                    <NavLink to='/login'>Login</NavLink>
-                </>
-            )}
+        
+         <div >
+            <p className='entry-header'></p>
+            <div className='navbar'>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/songs'>Music</NavLink>
+                <NavLink to='/band'>The Band</NavLink>
+                <NavLink to='/boards'>Boards</NavLink>
+                <NavLink to='/shows'>Shows</NavLink>
+                {user ? (
+                    <>
+                        <button onClick={handleLogOut}>Logout</button>
+                    </>
+                ) : (
+                    <>
+                        <NavLink to='/login'>Login</NavLink>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
