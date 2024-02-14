759a54f6dd15cf2b347ec02c14e9a0ae6349a1b6
emilyTheseond <esegundo02@gmail.com>
2024-02-14 14:45:47 -0600

    store members aray in data file

---
+++ src/data/band.js
@@ -0,0 +1,27 @@
+export const members = [
+    {
+      name: 'Julian Casablancas',
+      info: 'Julian Fernando Casablancas is an American musician. He is the lead vocalist and primary songwriter of the rock band The Strokes, with whom he has released six studio albums since their founding in 1998.',
+      imageUrl: 'https://media.gettyimages.com/id/1253383211/photo/julian-casablancas-of-the-strokes-performs-at-bill-graham-civic-auditorium-on-october-21-2003.jpg?s=612x612&w=0&k=20&c=l3FYSMwc2GvSWrj_oLcBAWEgV0mag_GGmznBb8kmB3Y='
+    },
+    {
+      name: 'Fabrizio Moretti',
+      info: 'Fabrizio Moretti is a Brazilian-American musician and visual artist best known as the drummer for American rock band The Strokes, with whom he has released six studio albums since 2001.',
+      imageUrl: 'https://imageio.forbes.com/specials-images/imageserve/5df7a5584e29170007832984/Benjamin-Lozovsky-BFA-com/960x0.jpg?format=jpg&width=1440'
+    },
+    {
+      name: 'Albert Hommond',
+      info: 'Albert Louis Hammond Jr. is an American musician who is a member of the rock band The Strokes. He is best known for his role as rhythm and lead guitarist, as well as occasionally a keyboard player and backing vocalist for the band. Hammond Jr. has released extensive solo work, including five solo albums.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Albert_Hammond_Jr._-_Palace_Theatre_-_St._Paul_-_First_Avenue_%28cropped%29.jpg/220px-Albert_Hammond_Jr._-_Palace_Theatre_-_St._Paul_-_First_Avenue_%28cropped%29.jpg'
+    },
+    {
+      name: 'Nick Valensi',
+      info: 'Nicholas Valensi is an American musician and songwriter, best known for his role as lead and rhythm guitarist in the American rock band The Strokes. Since 2001, the band has released six studio albums, some of which Valensi has also contributed keyboard and backing vocals.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Nick_valensi_2dollarbill.jpg'
+    },
+    {
+      name: 'Nikolai Fraiture',
+      info: 'Nikolai Philippe Fraiture is an American musician best known as the bassist of the rock band The Strokes. Since co-founding the band in 1998, he has released six studio albums with them.',
+      imageUrl: 'https://i.pinimg.com/736x/44/f0/83/44f083ac9822a30f887780709b59e63e.jpg'
+    }
+  ];
