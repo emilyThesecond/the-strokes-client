1bdd979a8082c004543122103d5f44592c5fd840
emilyTheseond <esegundo02@gmail.com>
2024-02-13 22:19:12 -0600

    song array

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,26 +1,33 @@
 const AudioPlayer = () => {
-    const videoId = 'pjmaj-wtAPs';
+    // const videoId = 'pjmaj-wtAPs';
     const songs = [
         { title: "You Only Live Once", videoId: "pjmaj-wtAPs"},
-        { title: "Selfless", videoId: "1-W6whvn8Bs"}
+        { title: "Selfless", videoId: "1-W6whvn8Bs"},
+        { title: "Ode to the Mets", videoId: "BjC0KUxiMhc"}
     ]
+    
   
     return (
       <div>
         <h1>Music</h1>
         <div>
-            <h2>I'll Try Anything Once</h2>
-        <iframe
+      {songs.map((song, index) => (
+        <div key={index}>
+          <h3>{song.title}</h3>
+          <iframe
           title="YouTube Video Player"
           width="50%"
           height="50"
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=1&loop=1&playlist=${videoId}&mute=1`}
+          src={`https://www.youtube.com/embed/${song.videoId}?autoplay=1&controls=1&loop=1&playlist=${song.videoId}&mute=1`}
           allow="autoplay; encrypted-media"
           className="youtube-song">
         </iframe>
+        </div>
+      ))}
+    </div>
 
         </div>
-      </div>
+
     );
   };
   
