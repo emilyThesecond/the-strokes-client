b8f46e9c50df63a4fb40e7753ebe2e4aa4195ada
emilyTheseond <esegundo02@gmail.com>
2024-02-14 23:00:49 -0600

    revert to original nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,41 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Nav = ({ user, handleLogOut }) => {
+    
     return (
-        <div className='nav-container'>
-            <div>
-                <h1 className='strokes-logo'>THE STROKES</h1>
-            </div>
+        
+        
+         <div className='nav-container'>
             <div className='entry-header'></div>
-            <div className='navbar-icons'>
-                <NavLink to='/'><i className="fas fa-home"></i></NavLink>
-                <NavLink to='/songs'><i className="fas fa-music"></i></NavLink>
-                <NavLink to='/band'><i className="fas fa-users"></i></NavLink>
-                <NavLink to='/boards'><i className="fas fa-chalkboard"></i></NavLink>
-                <NavLink to='/shows'><i className="fas fa-ticket-alt"></i></NavLink>
+            <div className='navbar'>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/songs'>Music</NavLink>
+                <NavLink to='/band'>The Band</NavLink>
+                <NavLink to='/boards'>Boards</NavLink>
+                <NavLink to='/shows'>Shows</NavLink>
                 {user ? (
                     <>
                         <button onClick={handleLogOut}>Logout</button>
                     </>
                 ) : (
                     <>
-                        <NavLink to='/login'><i className="fas fa-sign-in-alt"></i></NavLink>
-                    </>
-                )}
-            </div>
-            <div className='navbar-words'>
-                <NavLink to='/'>home</NavLink>
-                <NavLink to='/songs'>music</NavLink>
-                <NavLink to='/band'>the band</NavLink>
-                <NavLink to='/boards'>boards</NavLink>
-                <NavLink to='/shows'>shows</NavLink>
-                {user ? (
-                    <>
-                        <button onClick={handleLogOut}>Logout</button>
-                    </>
-                ) : (
-                    <>
-                        <NavLink to='/login'><i className="fas fa-sign-in-alt"></i></NavLink>
+                        <NavLink to='/login'>Login</NavLink>
                     </>
                 )}
             </div>
