62fdd8cfe18d9b77f77bed1b4186db0eea959843
emilyTheseond <esegundo02@gmail.com>
2024-02-14 23:17:04 -0600

    side slide out nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Nav = ({ user, handleLogOut }) => {
-    
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        
-        
-         <div className='nav-container'>
-            <div className='entry-header'></div>
+        <div className={`nav-container ${isOpen ? 'open' : ''}`}>
+            <div className='menu-toggle' onClick={toggleMenu}>
+                <i className="fas fa-bars"></i>
+            </div>
             <div className='navbar'>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/songs'>Music</NavLink>
-                <NavLink to='/band'>The Band</NavLink>
-                <NavLink to='/boards'>Boards</NavLink>
-                <NavLink to='/shows'>Shows</NavLink>
+                <NavLink to='/' onClick={toggleMenu}>Home</NavLink>
+                <NavLink to='/songs' onClick={toggleMenu}>Music</NavLink>
+                <NavLink to='/band' onClick={toggleMenu}>The Band</NavLink>
+                <NavLink to='/boards' onClick={toggleMenu}>Boards</NavLink>
+                <NavLink to='/shows' onClick={toggleMenu}>Shows</NavLink>
                 {user ? (
                     <>
                         <button onClick={handleLogOut}>Logout</button>
                     </>
                 ) : (
                     <>
-                        <NavLink to='/login'>Login</NavLink>
+                        <NavLink to='/login' onClick={toggleMenu}>Login</NavLink>
                     </>
                 )}
             </div>
